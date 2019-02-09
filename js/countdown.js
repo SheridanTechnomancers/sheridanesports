55dcbc5846ac235c1a6a62bbaf36f56866df0d2a
monamoe <33578294+monamoe@users.noreply.github.com>
2019-02-08 23:34:30 -0500

    JavaScript for the CountDown on the main page
    Script is disabled until we need the countdown on the main page

---
+++ js/countdown.js
@@ -0,0 +1,26 @@
+//for the countdown on the website's main page
+
+// ENTER THE DATE TO COUNT DOWN TO HERE //
+var countDownDate = new Date("Feb 31, 2019 15:00:00").getTime();
+
+//update
+var x = setInterval(function() {
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  //time between
+  var distance = countDownDate - now;
+  //formatting for time
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  //when countdown is finished
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Countdown finished.";
+  }
+}, 1000);
