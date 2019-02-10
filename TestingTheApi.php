9186fdde8078c1a193e5b2e770dcaf5efe5e5c69
Mythbran <matthewdangelo@hotmail.com>
2019-02-10 03:22:02 -0500

    Api work
    Teststed some more API. Need to pull specific data within the arrays but can pull match data. Sunjiv is the test account

--- TestingTheApi.php
+++ TestingTheApi.php
@@ -14,12 +14,24 @@ DataDragonAPI::initByCdn();
 //  Initialize the library
 $api = new LeagueAPI([
 	//  Your API key, you can get one at https://developer.riotgames.com/
-	LeagueAPI::SET_KEY    => 'RGAPI-c65166bf-abd8-49d4-993c-8e2e4bfbcfe7',
+	LeagueAPI::SET_KEY    => 'RGAPI-b25b32d4-2b4e-4907-87ad-b24303c097a5',
 	//  Target region (you can change it during lifetime of the library instance)
 	LeagueAPI::SET_REGION => Region::NORTH_AMERICA,
 ]);
 
 //  And now you are ready to rock!
-$champion = $api->getSummonerByName("HDFunctions");
-print_r($champion->getData());  //  Or array of all the data
+$account = $api->getSummonerByName("OSTRlCH");
+print_r($account->getData());  //  Or array of all the data
+
+//eventually we will be capturing ID 0 for custom games 
+//Needs to be mapid = 1
+$matchlist = $api->getMatchListByAccount('iYZAPyUiS5Cz53XHI1hiWJVv7akAkBFPeDgLgoAz76kOQQ', 420, null, null, null, null, null, null); 
+//print_r($matchlist);
+
+$matches = $api->getMatch('2972162655');
+print_r($matches[seasonId]["11"]);
+
+
+//[gameId] => 2974142280
+//( string $encrypted_account_id, $queue = null, $season = null, $champion = null, int $beginTime = null, int $endTime = null, int $beginIndex = null, int $endIndex = null )
 ?>
---
+++ php/LoLAnalytics/main.php
@@ -0,0 +1,29 @@
+<?php
+//  Include all required files
+require_once __DIR__  . "/dependencies/vendor/autoload.php";
+
+use RiotAPI\Exceptions\GeneralException;
+use RiotAPI\Objects\ChampionInfo;
+use RiotAPI\LeagueAPI\LeagueAPI;
+use RiotAPI\LeagueAPI\Definitions\Region;
+use RiotAPI\DataDragonAPI\DataDragonAPI;
+use RiotAPI\DataDragonAPI\Definitions\Map;
+
+DataDragonAPI::initByCdn();
+
+//  Initialize the library
+$api = new LeagueAPI([
+	//  Your API key, you can get one at https://developer.riotgames.com/
+	LeagueAPI::SET_KEY    => 'RGAPI-c65166bf-abd8-49d4-993c-8e2e4bfbcfe7',
+	//  Target region (you can change it during lifetime of the library instance)
+	LeagueAPI::SET_REGION => Region::NORTH_AMERICA,
+]);
+
+//  And now you are ready to rock!
+$champion = $api->getSummonerByName("HDFunctions");
+print_r($champion->getData());  //  Or array of all the data
+
+
+
+
+?>
