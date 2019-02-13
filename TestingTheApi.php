c5c300ff04718b96cca4880c7a3abe27210873f8
Mythbran <matthewdangelo@hotmail.com>
2019-02-13 00:15:27 -0500

    Updated API code
    I can now pull the latest game based o Summoner Name. Has a lot of test code and i will clean it up in a follow up push. For now it's somewhat working

--- TestingTheApi.php
+++ TestingTheApi.php
@@ -10,28 +10,82 @@ use RiotAPI\DataDragonAPI\DataDragonAPI;
 use RiotAPI\DataDragonAPI\Definitions\Map;
 
 DataDragonAPI::initByCdn();
-
+//EVAN'S CALLBACK CODE dWUxOnl1Yjh0eWJGYjF0RndrX3FwVEVwVHcuYXloaWlsT1NULXFXM1Z5WkNXaXd3dw%3D%3D
 //  Initialize the library
 $api = new LeagueAPI([
 	//  Your API key, you can get one at https://developer.riotgames.com/
-	LeagueAPI::SET_KEY    => 'RGAPI-b25b32d4-2b4e-4907-87ad-b24303c097a5',
+	LeagueAPI::SET_KEY    => 'RGAPI-68acb819-e350-466d-a62c-2712a63def54',
 	//  Target region (you can change it during lifetime of the library instance)
 	LeagueAPI::SET_REGION => Region::NORTH_AMERICA,
 ]);
 
-//  And now you are ready to rock!
-$account = $api->getSummonerByName("OSTRlCH");
-print_r($account->getData());  //  Or array of all the data
+$summonerName = "TAGUP Mythbran";
 
+//  And now you are ready to rock!
+$account = $api->getSummonerByName($summonerName); //WORKING. Needs to get summoner name from somwhere. Probably login dbase 
+//print_r($account->getData());  //  Or array of all the data
+
+//PRINT THE SUMMONER NAME 
+print_r($summonerName);
+echo "</br>";
+echo "</br>";
+echo "</br>";
+//PARAMETERS FOR MATCHLIST
 //eventually we will be capturing ID 0 for custom games 
 //Needs to be mapid = 1
-$matchlist = $api->getMatchListByAccount('iYZAPyUiS5Cz53XHI1hiWJVv7akAkBFPeDgLgoAz76kOQQ', 420, null, null, null, null, null, null); 
+
+//Matchlist prints out a 4d array 
+$matchlist = $api->getMatchListByAccount($account->accountId); //WORKING
 //print_r($matchlist);
 
-$matches = $api->getMatch('2972162655');
-print_r($matches[seasonId]["11"]);
+//print_r($matchlist->lane);
 
+$totalGames = $matchlist->totalGames;
 
-//[gameId] => 2974142280
-//( string $encrypted_account_id, $queue = null, $season = null, $champion = null, int $beginTime = null, int $endTime = null, int $beginIndex = null, int $endIndex = null )
+//print "totalGames: $totalGames";
+
+//$gameId = $matches->gameId;
+
+foreach($matchlist->matches as $game)
+	$gameIds[] = $game->gameId;
+
+/*for($i=0; $i<$totalGames; $i++){
+	print($gameIds[$i]);
+	echo "</br>";
+}
+*/
+
+$matchData = $api->getMatch($gameIds[0]);
+
+//print_r($matchData);
+$season = ($matchData->seasonId);
+//print "Season: $season";
+
+//participantIdentities I NEED THIS TO GET THE PARTICIPANTID. IT NEEDS TO BE LINKED TO THE SUMMONER NAME OR ACCOUNTID
+//print_r($matchData);
+foreach($matchData->participantIdentities as $participantIds){
+		$participant[] = $participantIds->player;
+
+}
+$participantId = 0;
+for($i=0; $i<10; $i++){
+	if($participant[$i]->accountId == $account->accountId)
+		$participantId = $i;
+	//print_r($participant[$i]->summonerName);
+	//echo "</br>";
+}
+//print"participantId: $participantId";
+
+$playerMatchData = $matchData->participants[$participantId-1];
+
+//print_r($playerMatchData);
+
+//KDA
+$kda = ($playerMatchData->stats->kills + $playerMatchData->stats->assists) / ($playerMatchData->stats->deaths);
+print "KDA: $kda";
+echo "</br>";
+//CS
+$cs = $playerMatchData->stats->totalMinionsKilled;
+print "CS: $cs";
+
 ?>
