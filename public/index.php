d714ceb852ec2ec04606c3fa0ea7b339ef54598e
Mythbran <matthewdangelo@hotmail.com>
2019-02-14 17:49:50 -0500

    Changed some stuff around to follow proper laravel formatting

--- public/index.php
+++ public/index.php
@@ -1,33 +1,60 @@
-<!doctype html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="author" content="Judd A.">
-        <meta name="viewport" content="width=device-width, initial-scale=1" >
-        <title>Sheridan Esports</title>
-        <link type="text/css" rel="stylesheet" href="css/mainPage.css">
-		<link rel="icon" href="images/favicon.png" type="image/png" />
-		<link rel="shortcut icon" href="images/favicon.png" type="image/png" /> 
-    </head>
-    <body>
-        <div class="layout">
-        <div class="header">
-            <img class="logo" src="images/sheridanlogo.png" alt="Sheridan Bruins Logo">
-	    <h2 id="countdown"></h2>
-            <a href="/login.html">login</a>
-        </div>
-        <div class="content">
-            <iframe src="https://www.youtube.com/embed/2xMQuMBh0Vo" frameborder="0" allowfullscreen>
-            </iframe>
-            <ul class="sidebar">
-                <!--<li> <a href="#">about</a></li>
-                <li> <a href="#">the teams</a></li>
-                <li> <a href="#">community</a></li>-->
-				<li> <a href="https://discord.gg/uJWVgkA"> <img class="social" src="images/discord.png" height="100vh" width="100vw"> </a> </li>
-            </ul>
-        </div>
-        </div>
-	 <!-- Script for countdown. Added Feb 8th 2019 
-        <script src="js/countdown.js"></script> -->
-    </body>
-</html>
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
