2ea750e87fe8beb8ddffeeaa3a37b1a371ff0995
Judd <44623501+ComfortablyCoding@users.noreply.github.com>
2019-03-03 12:21:58 -0500

    Calculates top 5 champs played in most recent 50 games
    Currently calculates the top 5 champs in terms of times played during the most recent 50 games.  First 50 atm due to error when attempting to go for 100 games. Stores results in two arrays top five champs and amount of times they have been played. Champs are stored as id num atm due to no call to dragon api to pull name. No account for champs that are played equal amount of times, stores the one that occurs fist.

---
+++ sheridananalytics/timesPlayedChamp.php
@@ -0,0 +1,90 @@
+<?php
+//  Include all required files
+require_once __DIR__  . "/../dependencies/vendor/autoload.php";
+
+use RiotAPI\Exceptions\GeneralException;
+use RiotAPI\Objects\ChampionInfo;
+use RiotAPI\LeagueAPI\LeagueAPI;
+use RiotAPI\LeagueAPI\Definitions\Region;
+use RiotAPI\DataDragonAPI\DataDragonAPI;
+use RiotAPI\DataDragonAPI\Definitions\Map;
+
+DataDragonAPI::initByCdn();
+//EVAN'S CALLBACK CODE dWUxOnl1Yjh0eWJGYjF0RndrX3FwVEVwVHcuYXloaWlsT1NULXFXM1Z5WkNXaXd3dw%3D%3D
+//  Initialize the library
+$api = new LeagueAPI([
+	//  Your API key, you can get one at https://developer.riotgames.com/
+	LeagueAPI::SET_KEY    => 'RGAPI-037f6e5f-6e3d-4a8f-b09d-5c52d5b58364',
+	//  Target region (you can change it during lifetime of the library instance)
+	LeagueAPI::SET_REGION => Region::NORTH_AMERICA,
+]);
+
+$summonerName = "scottlu"; //HARDCODED SUMMONER NAME
+$account = $api->getSummonerByName($summonerName); //WORKING. Needs to get summoner name from somwhere. Probably login dbase
+
+//needed initializations
+$matchlistSolo = $api->getMatchListByAccount($account->accountId, 420); //WORKING
+foreach($matchlistSolo->matches as $game){
+	if($game->lane == 'MID'){
+		$gameIds[] = $game->gameId;
+		$gameChampId[] = $game->champion;
+	}
+}
+
+//champ id array
+$champIdNumArr = array_fill(0, 100, -1);
+//top five used champ ids
+$topFiveChamps=array_fill(0,5,"null");
+//the amount of times the champs were used
+$timesChampsPlayed=array_fill(0,5,0);
+
+//finds the champ ids for most recent 100 games. Currently only pulls te most recent games due to error if try witth 100
+for ($j=0; $j<50; $j++){
+	$matchData = $api->getMatch($gameIds[$j]);
+	foreach($matchData->participantIdentities as $participantIds){
+		$participant[] = $participantIds->player;
+	}
+
+	for($i=0; $i<10; $i++){
+		if($participant[$i]->accountId == $account->accountId)
+		$participantId = $i;
+	}
+
+	//MOVES THE MATCHDATA ARRAY INTO IT'S OWN VARABLE. BREAKING UP THE ARRAY
+	$playerMatchData = $matchData->participants[$participantId];
+
+	//places each champion id in predefined array
+	$champIdNumArr[$j]=$playerMatchData->championId;
+}
+
+//finds the 5 champions played most
+//initializations of variables
+$count=1;
+$gamesPlayed=0;
+//iterates through champ id array and stores times champ is played
+for ($i=0;$i<sizeof($champIdNumArr);$i++){
+	$found=1;
+	//if champ id hasnt been checked yet, check it.
+	if($champIdNumArr[$i]!=-1){
+		$champIdNum=$champIdNumArr[$i];
+
+		//checks to see how many times it occurs
+		for($j=$i+1;$j<sizeof($champIdNumArr)-$j;$j++){
+			if($champIdNum== $champIdNumArr[$j]){
+				$gamesPlayed=$count++;
+				$champIdNumArr[$j]=-1;
+			}
+		}
+
+		//finds top 5 champs played.
+			for ($k=0;$k<sizeof($topFiveChamps);$k++){
+				if($gamesPlayed>$timesChampsPlayed[$k] && $found==1){
+					$topFiveChamps[$k]=$champIdNum;
+					$timesChampsPlayed[$k]=$gamesPlayed;
+					$found=0;
+				}
+		}
+		$count=1;
+	}
+}
+?>
